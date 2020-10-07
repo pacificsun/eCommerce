9d6815a6ad6c8ddb9006f3fd83f86a89ef97ad68
Suraj Thapa <stsurajst@gmail.com>
2020-10-07 08:33:43 +0545

    user routes added

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config(); // loads  .env files in process.env
 
+//import routes
+const userRoutes = require('./routes/user');
+
 // App
 const app = express();
 
@@ -10,13 +13,12 @@ mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('database conneted'));
 
-// Route
-app.get('/', (req, res) => {
-  res.send('Hello from node');
-});
+// Route middleware
+app.use('/api', userRoutes);
 
 const port = process.env.PORT || 8000;
 
